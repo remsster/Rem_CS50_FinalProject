57879e5c10833da6ec0b41557b161dd769347322
Rem <remitobias1@outlook.com>
2025-03-11 17:47:37 +0000

    Show user a message when validating task input

--- static/js/validate.js
+++ static/js/validate.js
@@ -1,11 +1,14 @@
 let taskInput = undefined;
 let submitTaskButton = undefined;
 let taskInputForm = undefined;
+let alertBar = undefined;
 
 document.addEventListener("DOMContentLoaded",() => {
     taskInput = document.querySelector("#task-input");
     submitTaskButton = document.querySelector("#submit-task-button");
     taskInputForm = document.querySelector("#task-input-form");
+    alertBar = document.querySelector("#warning");
+    alertBar.style.display = "none"
     
 
     taskInputForm.addEventListener("submit",(e) => {
@@ -21,28 +24,33 @@ function validateInput(text) {
     const beginningPattern = /^[a-zA-Z]+/;
     const endPattern =/[a-zA-Z]$/;
     const specialPattern = /^[^!@#$%^&*() -=+]+$/i;
-
     if (!beginningPattern.test(text)) {
-        console.error("Does not start with a letter");
+        showAlertBarWithMessage("Does not start with a letter");
         return false;
     }
 
     if (!endPattern.test(text)) {
-        console.error("Does not end with a letter");
+        
+        showAlertBarWithMessage("Does not end with a letter");
         return false;
     }
 
     if (!specialPattern.test(text)) {
-        console.error("Contains special characters");
+        showAlertBarWithMessage("Contains special characters, replace spaces with underscores _");
         return false;
     }
 
     for (let i = 0; i < mainTaskSelectTag.length; i ++) {
         if (text === mainTaskSelectTag[i].value) {
-            console.error("Task already exists");
+            showAlertBarWithMessage("Task already exists");
             return false;
         }
     }
 
     return true;
+}
+
+function showAlertBarWithMessage(message) {
+    alertBar.style.display = "block";
+    alertBar.innerHTML = message;
 }
